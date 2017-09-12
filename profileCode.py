f576ded3726856cd2cc2cea51e42a501fb18b413
Gabriela Jockel <gabriela.jockel@gmail.com>
2017-09-12 14:24:02 +0100

    things so far, not much to see, testing if the set up works

---
+++ profileCode.py
@@ -0,0 +1,19 @@
+#!/bin/python
+
+import sys
+import cProfile
+
+def do_cprofile(func):
+    def profiled_func(*args, **kwargs):
+        profile = cProfile.Profile()
+        try:
+            profile.enable()
+            result = func(*args, **kwargs)
+            profile.disable()
+            return result
+        finally:
+            profile.print_stats()
+    return profiled_func
+
+
+# garnish stuff to profile with: @do_cprofile
